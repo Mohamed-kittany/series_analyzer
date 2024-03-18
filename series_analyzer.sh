46f8e7ae8a01edae29b14f23987cbc555d64421c
Mohamed-kittany <kittanyenterprise@gmail.com>
2024-03-18 14:42:21 +0200

    version 1

--- series_analyzer.sh
+++ series_analyzer.sh
@@ -1,5 +1,105 @@
 #! /bin/bash
 
+function original_order()
+{
+	local arr_length=${#series[@]}
+
+	for ((i=0; i<$arr_length; i++))
+	do
+		echo -n "${series[$i]} "
+	done
+
+	echo
+}
+
+#NumberOfElemnts
+function num_of_elements()
+{
+    echo "Number of Elements in the Series : ${#series[@]}"
+}
+
+function sort() {
+    local sorted_array=("$@")
+    local length=${#sorted_array[@]}
+    
+    for ((i = 0; i < length; i++)); do
+        for ((j = 0; j < length-i-1; j++)); do
+            if [[ ${sorted_array[j]} -gt ${sorted_array[j + 1]} ]]; then
+                temp=${sorted_array[j]}
+                sorted_array[j]=${sorted_array[j + 1]}
+                sorted_array[j + 1]=$temp
+            fi
+        done
+    done
+
+    for ((m=0; m<length; m++)); do
+        echo -n "${sorted_array[$m]} "
+    done
+
+    echo
+}
+
+
+function max_value()
+{
+	local arr_length=${#series[@]}
+	local max=${series[0]}
+
+
+	for ((i=0; i<$arr_length; i++))
+	do
+		if (( series[$i] > $max ))
+		then
+			let max=${series[$i]}
+		fi
+	
+		done
+
+		echo "Maximum value is: $max"
+}
+
+
+
+function min_value()
+{
+    local arr_length=${#series[@]}
+    local min=${series[0]}
+
+    for ((i=0; i<$arr_length; i++))
+        do
+    		if (( series[$i] < $min ))
+            then
+			let min=${series[$i]}
+            fi
+        done
+    echo "Minimum value is: $min"
+}
+
+#sum
+function sum()
+{
+    local sum=0
+    for num in "${series[@]}"
+    do
+        local sum=$((sum+num))
+    done
+    #echo "Sum of the Series = $sum"
+}
+
+# average
+function average()
+{
+    local sum=0
+    for num in "${series[@]}"
+    do
+        sum=$((sum + num))
+    done
+    local average=$(printf "%.2f" "$(echo "scale=2; $sum / ${#series[@]}" | bc -l)")
+    echo "Average Of the Series=$average"
+}
+
+
+
 # Function to display the menu
 display_menu() {
     echo "Series Analyzer Menu:"
@@ -12,29 +112,93 @@ display_menu() {
     echo "g. Display the Number of elements in the series"
     echo "h. Display the Sum of the series"
     echo "i. Exit"
+    echo
 }
 
+# Function to validate the series
+# 
+validate_series() {
+    local series=("$@") # Local copy of the series array
+    local num
+
+    # Check for minimum count
+    if [ "${#series[@]}" -lt 3 ]; then
+        echo "Error: At least 3 positive numbers are required."
+        return 1 # Return 1 indicates error
+    fi
+
+    # check each element for being a positive number
+    for num in "${series[@]}"; do
+        # using regex to check if num is a positive integer
+        if ! [[ "$num" =~ ^[1-9][0-9]*$ ]]; then
+            echo "Error: Each input must be a positive number. Found invalid input: $num"
+            return 1 #  1 indicates error
+        fi
+    done
+
+    return 0 #  0 indicates success
+}
+
+# Function to read series from the user
+input_series() {
+    while true; do
+        echo "Enter a series of positive numbers separated by space ('exit' to quit):"
+        read -ra series
+        # Check if the user wants to exit
+        if [[ "${series[0]}" == "exit" ]]; then
+            echo "Exiting."
+            exit 0
+        fi
+        if validate_series "${series[@]}"; then
+            echo "Series accepted: ${series[*]}"
+            break
+        else
+            echo "Invalid input. Please try again."
+        fi
+    done
+}
+
+# Function to validate the series
+validate_series() {
+    local series=("$@")
+    local num
+
+    if [ "${#series[@]}" -lt 3 ]; then
+        echo "Error: At least 3 positive numbers are required."
+        return 1
+    fi
+
+    for num in "${series[@]}"; do
+        if ! [[ "$num" =~ ^[1-9][0-9]*$ ]]; then
+            echo "Error: Each input must be a positive number. Found invalid input: $num"
+            return 1
+        fi
+    done
+
+    return 0
+}
 
 # Main function
 main() {
+
     while true; do
         display_menu
         read -r choice
         case $choice in
             a) input_series ;;
-            b) display_series ;;
-            c) display_sorted_series ;;
-            d) calculate_max ;;
-            e) calculate_min ;;
-            f) calculate_average ;;
-            g) calculate_num_elements ;;
-            h) calculate_sum ;;
+            b) original_order $series;;
+            c) sort "${series[@]}" ;;
+            d) max_value $series ;;
+            e) min_value $series ;;
+            f) average $series;;
+            g) num_of_elements $series;;
+            h) sum $series;;
             i) echo "Exiting..."; exit ;;
             *) echo "Invalid option. Please choose a valid option." ;;
         esac
         echo
     done
 }
-
+input_series
 # Run the main function
 main
